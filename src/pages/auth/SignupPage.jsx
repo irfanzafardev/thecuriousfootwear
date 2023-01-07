51017631da45039f71bf3affddc8a4adf16beae5
“Irfan <irfanzafar.dev@gmail.com>
2023-01-08 02:15:27 +0700

    [DEV] update the folder sructure

--- src/pages/auth/SignupPage.jsx
+++ src/pages/auth/SignupPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
+import SignUpNavbar from "../../components/navbar/SignUpNavbar";
 import SignUpForm from "../../components/auth/signup/SignUpForm";
 import Footer from "../../components/footer/Footer";
-import SignUpNavbar from "../../components/navbar/SignUpNavbar";
 
 const SignUpPage = () => {
 	return (
